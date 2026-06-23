03ea107a7912d0cc1b0b64e1213f32e2ff414939
TahinaRAJ <tahinaraj1235@gmail.com>
2026-06-23 09:12:17 +0300

    tests2

--- myFramework/test/Servlet-Hello/src/main/java/test/exemple2/TestController.java
+++ myFramework/test/Servlet-Hello/src/main/java/test/exemple2/TestController.java
@@ -1,11 +1,12 @@
 package test.example2;
 
 import dev.framework.annotation.Controller;
+import dev.framework.annotation.UrlMapping;
 
 @Controller("/hello2")
 public class TestController {
 
-    @Controller("/world2")
+    @UrlMapping("/world2")
     public String world() {
         return "Hello world !";
     }
