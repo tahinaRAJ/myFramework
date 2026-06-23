c0458d6913545ef2ba2d147f6a288d20b2c399d0
TahinaRAJ <tahinaraj1235@gmail.com>
2026-06-23 08:32:05 +0300

    urlmapping

--- myFramework/framework/src/main/java/dev/framework/annotation/UrlMapping.java
+++ myFramework/framework/src/main/java/dev/framework/annotation/UrlMapping.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
-public @interface Controller {
+@Target(ElementType.METHOD)
+public @interface UrlMapping {
     String value() default "";
 }
